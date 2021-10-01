ef0efd7105c18cb2d847df6baab17ab02b2d31a8
slytherin20 <sonaligupta2904@gmail.com>
2021-10-01 16:46:13 +0530

    TS basics

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+// TS basics
+var char = "Mario";
+console.log(char + 40);
+char = "false";
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(4));
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+// TS basics
+let char = "Mario";
+
+console.log(char + 40);
+
+char = "false" ;
+
+const circ = (diameter: number)=>{
+    return diameter * Math.PI;
+}
+
+console.log(circ(4));
