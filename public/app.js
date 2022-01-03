79b38efb69d78cd1b0ae6efbf474aab025dc1784
slytherin20 <sonaligupta2904@gmail.com>
2022-01-03 16:55:03 +0530

    Classes

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 "use strict";
 //During development time TS does not know if there is some kind of anchor tag in the html.
-//Therefore, trying to access any property of a tag will throw error.
-// const anchor = document.querySelector(a)
+//Therefore, trying to access any property of 'a' tag will throw error.
+//const anchor = document.querySelector(a)
 //console.log(a.href) //throws error
 //Solution:
 var anchor = document.querySelector('a');
@@ -15,3 +15,39 @@ form1.addEventListener("submit", function (e) {
     console.log(input.value);
     console.log(amount.valueAsNumber);
 });
+//Classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes " + this.amount + " for" + this.details + " ";
+    };
+    return Invoice;
+}());
+var inv1 = new Invoice("Mario", "for remaining bill", 2500);
+var inv2 = new Invoice("Sam", "for fruits", 100);
+console.log(inv1, inv2);
+console.log(inv1.format());
+var invoices = [];
+invoices.push(inv1, inv2);
+console.log(invoices);
+//Another way to write class when access specifiers are present. Important: only when access specifiers are present.
+var InvoiceOne = /** @class */ (function () {
+    //  readonly client:string;  //Allows only read inside and outside the class.
+    // private details:string;  //Allows accessing the property only inside the class.
+    // public amount:number;    //Default. Allows access to property both inside and outside the class.
+    function InvoiceOne(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    InvoiceOne.prototype.format = function () {
+        return this.client + " owes " + this.amount + " for" + this.details + " ";
+    };
+    return InvoiceOne;
+}());
+var inv3 = new InvoiceOne("Sammy", "for goods", 2000);
+console.log(inv3.format());
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 //During development time TS does not know if there is some kind of anchor tag in the html.
-//Therefore, trying to access any property of a tag will throw error.
-// const anchor = document.querySelector(a)
+//Therefore, trying to access any property of 'a' tag will throw error.
+//const anchor = document.querySelector(a)
 //console.log(a.href) //throws error
 
 //Solution:
@@ -20,3 +20,55 @@ form1.addEventListener("submit",(e:Event)=>{
     console.log(input.value)
     console.log(amount.valueAsNumber)
 })
+
+//Classes
+class Invoice{
+    readonly client:string;  //Allows only read inside and outside the class.
+    private details:string;  //Allows accessing the property only inside the class.
+    public amount:number;    //Default. Allows access to property both inside and outside the class.
+
+    constructor(c:string,d:string,a:number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format(){
+        return `${this.client} owes ${this.amount} for${this.details} `
+    }
+}
+
+const inv1 = new Invoice("Mario","for remaining bill",2500)
+
+const inv2 = new Invoice("Sam","for fruits",100)
+
+console.log(inv1,inv2)
+console.log(inv1.format())
+
+let invoices: Invoice[] = [];
+invoices.push(inv1,inv2)
+
+console.log(invoices)
+
+//Another way to write class when access specifiers are present. Important: only when access specifiers are present.
+
+class InvoiceOne{
+  //  readonly client:string;  //Allows only read inside and outside the class.
+   // private details:string;  //Allows accessing the property only inside the class.
+   // public amount:number;    //Default. Allows access to property both inside and outside the class.
+
+    constructor(
+        readonly client:string,
+        private details:string,
+        public amount:number
+    ){
+    }
+
+    format(){
+        return `${this.client} owes ${this.amount} for${this.details} `
+    }
+}
+
+const inv3 = new InvoiceOne("Sammy","for goods",2000)
+
+console.log(inv3.format())
